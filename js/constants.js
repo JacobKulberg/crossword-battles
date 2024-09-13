0faeafd31d167dbb635d4c963e66d9a5489b8611
JACOB-COMPUTER\jacob <jacobkulberg@gmail.com>
2024-09-13 13:57:22 -0400

    Toggled off debug mode for main release

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-const DEBUG = true;
+const DEBUG = false;
 
 const originalConsoleLog = console.log;
 console.log = function (...args) {
